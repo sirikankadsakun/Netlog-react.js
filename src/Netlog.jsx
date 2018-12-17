369130ad3234de9c5861eaa2869666c3363b4196
unknown <sirikan29>
2018-12-17 11:16:17 +0700

    first commit

---
+++ src/Netlog.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Netlog.css';
+///import './components/Appheader';
+import Appheader from './components/Appheader';
+
+class Netlog extends Component{
+    state = {}
+    render(){
+        return ( 
+            <div>
+            <Appheader />
+            <h1>Net Log Applicaion</h1>
+            <h1>Create by. Sirikan</h1>
+            
+            </div>
+        );
+    }
+}
+
+export default Netlog;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import $ from 'jquery/dist/jquery.slim';
+import popper from 'popper.js/dist/popper';
+import 'bootstrap/dist/js/bootstrap.bundle';
+import Netlog from './Netlog';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Netlog />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
